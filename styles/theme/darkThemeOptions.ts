94f01643c399ac0f4f62a752fea24cc187a60332
xxibcill <36674072+xxibcill@users.noreply.github.com>
2022-08-27 23:09:05 +0700

    init darkTheme

---
+++ styles/theme/darkThemeOptions.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+const darkThemeOptions: ThemeOptions = {
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: "#cf0029"
+        },
+        secondary: {
+            main: "#e98195",
+            light: "#ffffff"
+        }
+    },
+};
+
+export default darkThemeOptions;
